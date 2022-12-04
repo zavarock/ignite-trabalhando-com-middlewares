e2faf02b35c96858c82f0786bcbec5aa2077ceb0
Josenilton Junior <junior.coh@gmail.com>
2022-12-04 16:47:17 -0300

    Done

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ 'error': 'User does not exists!' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ 'error': 'To create more TODOS, you need a PRO plan!' });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  if (!validate(id)) {
+    return response.status(400).json({ 'error': 'Invalid UUID.' });
+  }
+
+  let { user } = request;
+  if (user == null) {
+    user = users.find(user => {
+      const todoExists = user.todos.some(todo => todo.id === id);
+
+      return todoExists;
+    });
+  }
+
+  if (!user) {
+    return response.status(404).json({ 'error': 'User not found.' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ 'error': 'TODO not found.' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return response.status(404).json({ 'error': 'User not found.' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
